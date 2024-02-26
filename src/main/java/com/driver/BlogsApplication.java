22e0db30b84b873368f668cbc2a153348b472fb7
Rohan859 <rohankundu859@gmail.com>
2024-02-27 00:09:26 +0530

    delete method changed

--- src/main/java/com/driver/BlogsApplication.java
+++ src/main/java/com/driver/BlogsApplication.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogsApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogsApplication.class, args);
+        
     }
 }
